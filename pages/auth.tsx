393097bb5e5da41009faa6137e0af9370ac0d427
Edmerson Pizarra <edmerpizarra@gmail.com>
2021-12-14 11:48:44 +0800

    FIX: build error

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,17 +3,19 @@ import React, { useEffect, useState } from 'react'
 export default function Auth() {
     const [token, setToken] = useState<string>('');
     useEffect(() => {
-        const parsedHash = new URLSearchParams(
-            window.location.hash.substr(1) // skip the first char (#)
-          );
-        let parse: string = parsedHash.get("access_token") ?? '';
-        setToken(parse);//krazy
-        localStorage.setItem('token',token);
         
+        if(typeof window !== 'undefined') {
+            const parsedHash = new URLSearchParams(
+                window.location.hash.substr(1) // skip the first char (#)
+            );
+            let parse: string = parsedHash.get("access_token") ?? '';
+            setToken(parse);//
+                localStorage.setItem('token',token);
+        }
     }, [token])
     return (
         <div>
-            
+            {token}
         </div>
     )
 }
