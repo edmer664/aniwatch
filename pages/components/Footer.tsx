73c546561387672729cc2b0d05f452472fd5ae9a
Edmerson Pizarra <edmerpizarra@gmail.com>
2021-12-16 21:57:05 +0800

    update: revise disclaimer in the footer

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className='bg-slate-700 text-white text-center h-max flex flex-col justify-around'>
-            <span className='py-3'>
-                <strong>Disclaimer:</strong> AniWatch is a no-profit project, we are not affiliated with<br/>
-                Anilist or MyAnimeList and we do not host illegal files.
+            <span className='py-3 px-10 text-xs xl:text-sm text-justify'>
+                <strong>Disclaimer:</strong> AniWatch is a no-profit project, All of the free resources found on this app are hosted on third-party servers
+                that are freely available to watch online for all internet users. Any legal issues regarding the free Anime should be taken up with the actual
+                file hosts themselves, as we're not affiliated with them. Copyrights and trademarks for the LN, and other promotional materials are held by
+                their respective owners and their use is allowed under the fair use clause of the Copyright Law.
             </span> 
             <span className='py-3 flex justify-center'>
                 <img src="/img/aniapi_icon.png" alt="aniapi" className='w-5'/>
