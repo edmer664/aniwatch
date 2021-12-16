2283d7344703e739d90d808480662134aaf8d6e7
Edmerson Pizarra <edmerpizarra@gmail.com>
2021-12-16 12:32:14 +0800

    feat: add genre to BigCard component

--- pages/components/BigCard.tsx
+++ pages/components/BigCard.tsx
@@ -7,13 +7,14 @@ export default function BigCard(
         img_alt: string,
         title: string,
         desc: string,
-        link: string
+        link: string,
+        genres: [string],
 
 }) {
     
 
     return (
-        <div className='flex bg-slate-600 text-white rounded-2xl w-9/12 h-56 hover:ring-gray-400 hover:ring-2'>
+        <div className='flex bg-slate-600 text-white rounded-2xl w-9/12 h-72 hover:ring-gray-400 hover:ring-2'>
             <div className='w-1/3 rounded-l-2xl h-full relative overflow-hidden'>      
                 <Image
                 src={props.img_url}
@@ -24,12 +25,23 @@ export default function BigCard(
             </div>
             <div className='flex flex-col w-2/3 px-5 text-justify'>
 
-                <h2 className='font-bold text-3xl pb-5 hover:text-slate-300'>
+                <h2 className='font-bold text-3xl pb-5 hover:text-slate-300 '>
                     <a href={props.link}>{props.title}</a>
                 </h2>
                 <p>
                     {props.desc}
                 </p>
+                <div className='flex flex-wrap'>
+                    {props.genres.map(
+                        (genre) => (
+                            <div className='p-1.5 bg-gray-400 rounded-md hover:bg-gray-500 m-1 duration-100 transition-all'>
+                                <a href={`/Explore#genre=${genre}`}>
+                                    {genre}
+                                </a>
+                            </div>
+                        )
+                    )}
+                </div>
             </div>
         </div>
     )
