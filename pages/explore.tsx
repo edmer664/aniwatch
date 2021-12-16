7e4bc76e856375a4ab07337a43d6e60f50925600
Edmerson Pizarra <edmerpizarra@gmail.com>
2021-12-16 13:11:16 +0800

    feat: add explore page to browse anime

---
+++ pages/explore.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+
+export default function Explore() {
+    return (
+        <React.Fragment>
+            <Navbar/>
+            <div>
+                <span>
+                    UNDER DEVELOPMENT
+                </span>
+            </div>
+            <Footer/>
+        </React.Fragment>
+    )
+}
