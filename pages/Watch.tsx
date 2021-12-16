ddb845ae10b60318fd9207f56a4d280b3e4be392
Edmerson Pizarra <edmerpizarra@gmail.com>
2021-12-16 21:58:34 +0800

    feat: add loading spinner and responsive design

--- pages/Watch.tsx
+++ pages/Watch.tsx
@@ -5,12 +5,14 @@ import Navbar from './components/Navbar';
 const ANIAPI = require("@mattplays/aniapi");
 import Image from 'next/image';
 import SmallCard from './components/SmallCard';
+import { PulseLoader } from 'react-spinners';
 
 export default function Watch() {
     const [token, setToken] = useState<string>('');
     const [data, setData] = useState<any>();
     const [seasonW, setSeasonW] = useState<string>();
     const [year, setYear] = useState<any>();
+    const [loading, setLoading] = useState<boolean>(true);
     const API = new ANIAPI.API(token);
     
     useEffect(() => {
@@ -21,7 +23,6 @@ export default function Watch() {
             );
         }
         
-        
         const yeartd = new Date().getFullYear();
         const getSeason = (d: Date) => Math.floor((d.getMonth() / 12 * 4)) % 4;
         const currSeason = getSeason(new Date());
@@ -29,18 +30,13 @@ export default function Watch() {
         setSeasonW(seasonInWords(currSeason));
         setYear(yeartd);
 
+        // fetchicng data
         API.Anime.Get({
             year:yeartd,
             season:currSeason,
         }).then((res:any) => {
-            
             setData(res);
-            console.log(res)
-            console.log("______________")
-            console.log(data);
-            console.log("working");
-            console.log(data);
-            
+            setLoading(false);
         });
         
         
@@ -48,7 +44,7 @@ export default function Watch() {
     return (
         <div className='bg-black w-screen h-screen'>
             <Navbar/>
-                <main className='flex flex-col text-white items-center py-10 justify-center'>
+                <main className='flex flex-col bg-black text-white items-center py-10 justify-center'>
                     <div className='flex'>
                         <div className='relative w-12'>
                             <Image
@@ -58,10 +54,17 @@ export default function Watch() {
                                 alt="chibi thumbs up"
                             />
                         </div>
-                        <h1 className='font-bold text-5xl py-5'>
+                        <h1 className='font-bold xl:text-5xl py-5 text-2xl'>
                             New Anime!
                         </h1>
                     </div>
+                    {
+                    <PulseLoader
+                    color='#ffffff'
+                    loading={loading}
+                    size={15}
+                    />
+                    }
                     {data &&
                     <BigCard
                     img_url={data.data.documents[0].banner_image}
@@ -76,11 +79,28 @@ export default function Watch() {
 
 
                 </main>
-                <section className='flex flex-col items-center p-20 w-full bg-black'>
-                    <h2 className='text-white text-3xl py-5 font-semibold'>
-                        {seasonW} Anime {year}
-                    </h2>
-                    <div className='flex flex-wrap w-full'>
+                <section className='flex flex-col items-center p-5 xl:p-20 w-full bg-black'>
+                    <div className='flex'>
+                    <div className='relative w-8'>
+                            <Image
+                                layout='fill'
+                                objectFit="contain"
+                                src='/img/thumsup.png'
+                                alt="chibi thumbs up"
+                            />
+                        </div>
+                        <h2 className='text-white text-3xl py-5 font-semibold'>
+                            {seasonW} Anime {year}
+                        </h2>
+                    </div>
+                    {
+                    <PulseLoader
+                    color='#ffffff'
+                    loading={loading}
+                    size={15}
+                    />
+                    }
+                    <div className='flex flex-wrap justify-evenly w-full'>
                         {data && 
                             data.data.documents.map(
                                 (anime: any) => (
